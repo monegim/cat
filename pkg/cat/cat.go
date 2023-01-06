4688d9b6e41979e6bea97a68a23c5a7dc228af63
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-06 22:56:32 +0330

    update cat method for 1 input

--- pkg/cat/cat.go
+++ pkg/cat/cat.go
@@ -1,13 +1,33 @@
 package cat
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
-// type 
-func Cat (cmd *cobra.Command, args []string) {
-	for i, arg := range args {
-		fmt.Printf("%d, %s\n", i, arg)
+
+// type
+func Cat(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		fmt.Println("no file provided")
+		os.Exit(1)
+	}
+	if len(args) == 1 {
+		content, err := os.Open(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer content.Close()
+		scanner := bufio.NewScanner(content)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
