804a92893eb6939c8845d7499bcf9db4f7fa5e3d
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-06 21:54:55 +0330

    add Execute

--- cmd/cat/root.go
+++ cmd/cat/root.go
@@ -1,12 +1,24 @@
 package cat
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
-	Use: "cat",
+	Use:   "cat",
 	Short: "cat - cat a file",
-	Long: "It acts like linux cat",
+	Long:  "It acts like linux cat",
 	Run: func(cmd *cobra.Command, args []string) {
-		
+
 	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
