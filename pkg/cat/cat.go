887780d540dbf7540f72fd314b34ce9bc989d091
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-07 12:23:25 +0330

    update for loop

--- pkg/cat/cat.go
+++ pkg/cat/cat.go
@@ -13,26 +13,26 @@ func Cat(cmd *cobra.Command, args []string) {
 	if len(args) == 0 {
 		fmt.Println("no file provided")
 		os.Exit(1)
-	} else{
-	for _, path := range(args) {
-		readFile(path)
+	} else {
+		for i := 0; i < len(args); i++ {
+			readFile(args[i])
+		}
 	}
 }
-}
 
-func readFile(name string)  {
+func readFile(name string) {
 	content, err := os.Open(name)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer content.Close()
-		scanner := bufio.NewScanner(content)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer content.Close()
+	scanner := bufio.NewScanner(content)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
